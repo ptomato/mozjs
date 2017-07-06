0231f76f27266c62a5b41bb61fe8e0008790da5f
Philip Chimento <philip.chimento@gmail.com>
2017-07-05 22:57:09 -0700

    build: Be nicer about options
    We have to account for JHbuild passing extra unknown
    options like --disable-Werror.
    
    This has been rejected upstream and is only needed for GNOME Continuous
    which passes --build automatically. JHbuild uses
    supports-unknown-configure-options although it is debatable whether we
    want to keep that.

--- python/mozbuild/mozbuild/configure/options.py
+++ python/mozbuild/mozbuild/configure/options.py
@@ -402,7 +402,11 @@ class CommandLineHelper(object):
 
     def add(self, arg, origin='command-line', args=None):
         assert origin != 'default'
-        prefix, name, values = Option.split_option(arg)
+        try:
+            prefix, name, values = Option.split_option(arg)
+        except InvalidOptionError as e:
+            print('Ignoring', arg, ':', e)
+            return
         if args is None:
             args = self._extra_args
         if args is self._extra_args and name in self._extra_args:
